0769d305a81500556beacd4d6eeb90014d158f29
pearcej <jan_pearce@berea.edu>
2019-10-25 06:24:38 -0400

    add files, Getrandom

--- mondrian/main-mondrian.cpp
+++ mondrian/main-mondrian.cpp
@@ -1,11 +1,31 @@
  /* Authors: 
     Python version of Sierpinski's triangle by Jan Pearce; Conversion to C++ by Jesse W. Walker
-	C-Turtle by Jesse  W.  Walker
+	C-Turtle by Jesse  W.  Walker, Getrandom by Jan Pearce
 
  */
 
 #include "CTurtle.hpp"   //This brings in the CTurtle library for use
+#include <iostream> //for input & output
+#include <random> //needed for Getrandom
+#include <chrono> //needed for Getrandom's seed
 namespace ct = cturtle;  //This makes it possible to use the CTurtle commands using ct::
+using namespace std;
+
+class Getrandom {
+	/** Uses <random> and <chrono> from C++11 to return a random integer in range [1..size] */
+public:
+	Getrandom(int size) {
+		auto seed = chrono::system_clock::now().time_since_epoch().count(); //gets a new seed for the randomness
+		default_random_engine generator(seed);			//seeds our randomness
+		uniform_int_distribution<int> intdist(1, size); //a distibution to make each in-range integer equally likely
+		self_rand_int_ = intdist(generator);			//generates the randme number
+	}
+	int roll() {
+		return self_rand_int_;
+	}
+private:
+	int self_rand_int_;
+};
 
 
 void draw_triangle(ct::Point a, ct::Point b, ct::Point c, ct::Color color, ct::Turtle& myTurtle){
@@ -35,10 +55,12 @@ void sierpinski(ct::Point a, ct::Point b, ct::Point c, int degree, ct::Turtle& m
 int main() {
     ct::TurtleScreen scr; //makes screen
     ct::Turtle rt(scr);   //makes Turtle on screen
+
+	Getrandom newrandom(4);
     
 	//graphing commands go below here
     ct::Point myPoints[] = {{-200, -100}, {0, 200}, {200, -100}};
-    sierpinski(myPoints[0], myPoints[1], myPoints[2], 3, rt);
+    sierpinski(myPoints[0], myPoints[1], myPoints[2], newrandom.roll(), rt);
     
     scr.exitonclick();  //exists graphics screen
     return 0;
