40af6e71f9db66795783c88ad72a42300dbb6c88
Jan Pearce <pearcej@gmail.com>
2020-09-10 21:50:00 -0400

    improve comment on TurtleScreen.tracer()

--- main.cpp
+++ main.cpp
@@ -54,7 +54,7 @@ void sierpinski(ct::Point a, ct::Point b, ct::Point c, int degree, ct::Turtle& m
 
 int main() {
     ct::TurtleScreen scr; //makes screen
-    turtleScreen.tracer(0); //improves speed
+    scr.tracer(20); //improves speed the most with parameter 0, greater to see what is happening
     ct::Turtle rt(scr);   //makes Turtle on screen
 
 	Getrandom newrandom(4);
