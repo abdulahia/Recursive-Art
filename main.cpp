624d3dbc294a3c0661095dcd55bc09e9a32dc596
torresverae <torresverae@berea.edu>
2020-11-16 03:19:12 +0000

    9.5

--- main.cpp
+++ main.cpp
@@ -21,29 +21,33 @@ private:
 	int self_rand_int_;
 };
 
-    void draw_rectangle(ct::Point a, ct::Point b, ct::Point c, ct::Point d, ct::Turtle& myTurtle){
-    // myTurtle.fillcolor(color);
+void draw_rectangle(ct::Point a, ct::Point b, ct::Point c, ct::Point d, ct::Turtle& myTurtle) {
+        Getrandom rant(6);
+        const string colormap[] = { "blue", "red", "green", "white", "yellow", "violet", "orange" };
+        string color = colormap[rant.roll()];
+    myTurtle.fillcolor(color);
     myTurtle.penup();
     myTurtle.goTo(a.x, a.y);
     myTurtle.pendown();
+    myTurtle.begin_fill();
     myTurtle.goTo(b.x, b.y);
     myTurtle.goTo(c.x, c.y);
     myTurtle.goTo(d.x, d.y);
     myTurtle.goTo(a.x, a.y);
-    // myTurtle.end_fill();
+    myTurtle.end_fill();
     }
     
     void mondrian(ct::Point a, ct::Point b, ct::Point c, ct::Point d, ct::Turtle& myTurtle){
-      const string colormap[] = {"blue", "red", "green", "white", "yellow", "violet", "orange"};
+
     draw_rectangle(a,b,c,d, myTurtle);
     
     // The recursive mondrian function that holds all the steps found above.
 
 
     int width = b.x - a.x;
-    cout << "Here is the width " << width;
+    cout << "\nHere is the width: " << width << endl;
     int height = a.y - d.y;
-    cout << "Here is the height " << height;
+    cout << "\nHere is the height: " << height << endl;
     
     Getrandom ran(width);
     Getrandom other_ran(height);
@@ -51,34 +55,42 @@ private:
     int randy = other_ran.roll() + d.y;
 
     if (width > 800/2 && height > 600/2){
-      mondrian(a, {randx, a.y}, {randx, d.y},{d.x, d.y}, myTurtle);
+      mondrian(a, { randx, a.y }, { randx, d.y }, { d.x, d.y }, myTurtle);
       mondrian({a.x, randy},{b.x, randy},{c.x, c.y} , {d.x, d.y}, myTurtle);
-  
+      mondrian({ randx,a.y}, b , c , { randx, d.y}, myTurtle);
+      mondrian({ a.x, a.y }, { b.x, b.y }, { c.x, randy }, { d.x, randy }, myTurtle);
+
     }
     else if (width > 800/2){
-      mondrian(a, {randx, a.y}, {randx, d.y},{d.x, d.y}, myTurtle);
+        mondrian(a, { randx, a.y }, { randx, d.y }, { d.x, d.y }, myTurtle);
+        mondrian({ randx,a.y }, b, c, { randx, d.y }, myTurtle);
     }
     else if (height > 600/2){
-    mondrian({a.x, randy},{b.x, randy},{c.x, c.y} , {d.x, d.y}, myTurtle);
+    mondrian({ a.x, randy }, { b.x, randy }, { c.x, c.y }, { d.x, d.y }, myTurtle);
+    mondrian({ a.x, a.y }, { b.x, b.y }, { c.x, randy }, { d.x, randy }, myTurtle);
     }
-   else if (width > 40 && height > 40){
-     mondrian(a, {randx, a.y}, {randx, d.y},{d.x, d.y}, myTurtle);
-    mondrian({a.x, randy},{b.x, randy},{c.x, c.y} , {d.x, d.y}, myTurtle);
+   else if (width > 60 && height > 60){
+        //mondrian(a, { randx, a.y }, { randx, d.y }, { d.x, d.y }, myTurtle);
+        //mondrian({ a.x, randy }, { b.x, randy }, { c.x, c.y }, { d.x, d.y }, myTurtle);
+        //mondrian({ randx,a.y }, b, c, { randx, d.y }, myTurtle);
+        //mondrian({ a.x, a.y }, { b.x, b.y }, { c.x, randy }, { d.x, randy }, myTurtle);
    }
-   else if(width > 40){
-    mondrian(a, {randx, a.y}, {randx, d.y},{d.x, d.y}, myTurtle);
+   else if(width > 150){
+        //mondrian(a, { randx, a.y }, { randx, d.y }, { d.x, d.y }, myTurtle);
+        //mondrian({ randx,a.y }, b, c, { randx, d.y }, myTurtle);
    }
-   else if(height > 40){
-    mondrian({a.x, randy},{b.x, randy},{c.x, c.y} , {d.x, d.y}, myTurtle); 
+   else if(height > 150){
+        //mondrian({ a.x, randy }, { b.x, randy }, { c.x, c.y }, { d.x, d.y }, myTurtle);
+        //mondrian({ a.x, a.y }, { b.x, b.y }, { c.x, randy }, { d.x, randy }, myTurtle);
    }
   }
     
 int main(){
 ct::TurtleScreen scr; //makes screen
-scr.tracer(2); //improves speed the most with parameter 0, greater to see what is happening
+scr.tracer(1); //improves speed the most with parameter 0, greater to see what is happening
 ct::Turtle rt(scr);   //makes Turtle on screen
 
-
+Getrandom newrandom(7);
 ct::Point Points[] = {{-400,300 }, {400, 300}, {400, -300},{-400, -300}};
 mondrian(Points[0], Points[1], Points[2], Points[3], rt);
 scr.exitonclick();  
